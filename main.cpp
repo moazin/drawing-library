a0de6a913d0e20cea5b55993c7a0625bb54ac548
Moazin Khatri <moazinkhatri@gmail.com>
2019-03-09 19:39:16 +0500

    first function plot

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 
 using namespace std;
 
+float function(float x){
+    return (x*x)/1000;
+}
+
 int main(void){
     Canvas canvas{1000, 1000};
 
     Rectangle rect{Point(100, 100), Point(900, 900)};
     canvas.draw(&rect);
-
-    Line line{Point(100, 100), Point(900, 900)};
-    canvas.draw(&line);
-
-    Dot dot{Point(100, 950)};
-    canvas.draw(&dot);
-
+    for(float x = 0; x <= 800; x += 1){
+        float yDot = function(x) + 100;
+        float xDot = x + 100;
+        if(!((xDot < 100) || (xDot > 900) || (yDot < 100) || (yDot > 900))){
+            Dot dot{Point(xDot, yDot)};
+            canvas.draw(&dot);
+        }
+    }
     CanvasDrawerWindow canvasDrawerWindow(canvas);
     canvasDrawerWindow.draw();
     return 0;
